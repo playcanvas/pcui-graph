af4b447a275ef6b1d357488aff533d770cbaf98d
Will Eastcott <will@playcanvas.com>
2023-03-21 12:59:35 +0000

    Switch to first party rollup terser plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
 import resolve from '@rollup/plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
 import postcss from 'rollup-plugin-postcss';
 import commonjs from '@rollup/plugin-commonjs';
 import { babel } from '@rollup/plugin-babel';
+import terser from '@rollup/plugin-terser';
 import jscc from 'rollup-plugin-jscc';
 import builtins from 'rollup-plugin-node-builtins';
 import globals from 'rollup-plugin-node-globals';
