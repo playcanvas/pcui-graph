e8fed78ac680a875476a745344654de2946f5814
Will Eastcott <will@playcanvas.com>
2025-02-12 09:46:10 +0000

    Update from Storybook 7 to 8

--- .storybook/main.js
+++ .storybook/main.js
@@ -28,7 +28,5 @@ module.exports = {
     options: {}
   },
 
-  docs: {
-    autodocs: true
-  }
+  docs: {}
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,20 +1,22 @@
 const preview = {
-  parameters: {
-      backgrounds: {
-          default: 'playcanvas',
-          values: [
-              {
-                  name: 'playcanvas',
-                  value: '#374346'
-              },
-              {
-                  name: 'white',
-                  value: '#FFFFFF'
-              }
-          ]
-      },
-      controls: { expanded: true }
-  }
+    parameters: {
+        backgrounds: {
+            default: 'playcanvas',
+            values: [
+                {
+                    name: 'playcanvas',
+                    value: '#374346'
+                },
+                {
+                    name: 'white',
+                    value: '#FFFFFF'
+                }
+            ]
+        },
+        controls: { expanded: true }
+    },
+
+    tags: ['autodocs']
 };
 
 export default preview;
