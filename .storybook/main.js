25d576da31a203417ab0b55cff31a86ee61ae42b
Will Eastcott <will@playcanvas.com>
2025-02-20 20:04:35 +0000

    Reformat Storybook main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,32 +1,33 @@
-module.exports = {
-  stories: ['./**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+const config = {
+    stories: ['./**/*.stories.@(js|jsx|ts|tsx|mdx)'],
 
-  addons: [
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    '@storybook/addon-docs',
-    '@storybook/addon-backgrounds/register',
-    '@storybook/preset-create-react-app'
-  ],
+    addons: [
+        '@storybook/addon-actions',
+        '@storybook/addon-links',
+        '@storybook/addon-docs',
+        '@storybook/addon-backgrounds/register',
+        '@storybook/preset-create-react-app'
+    ],
 
-  webpackFinal: async (config, { configType }) => {
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
+    webpackFinal: async (config, { configType }) => {
+        // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+        // You can change the configuration based on that.
+        // 'PRODUCTION' is used when building the static version of storybook.
 
-    config.module.rules = config.module.rules.filter(rule => {
-        if (!rule.test) return true;
-        return !rule.test.test(".scss");
-    });
+        config.module.rules = config.module.rules.filter(rule => {
+            if (!rule.test) return true;
+            return !rule.test.test(".scss");
+        });
 
-    // Return the altered config
-    return config;
-  },
+        return config;
+    },
 
-  framework: {
-    name: '@storybook/react-webpack5',
-    options: {}
-  },
+    framework: {
+        name: '@storybook/react-webpack5',
+        options: {}
+    },
 
-  docs: {}
+    docs: {}
 };
+
+export default config;
