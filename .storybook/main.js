def54b152450e6fba7ee42fdd32e4997e215a833
Will Eastcott <will@playcanvas.com>
2025-02-20 20:59:44 +0000

    Remove @storybook/preset-create-react-app

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,19 +2,34 @@ const config = {
     stories: ['./**/*.stories.@(js|jsx|ts|tsx|mdx)'],
 
     addons: [
-        '@storybook/addon-essentials',
-        '@storybook/preset-create-react-app'
+        '@storybook/addon-essentials'
     ],
 
     webpackFinal: async (config, { configType }) => {
-        // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-        // You can change the configuration based on that.
-        // 'PRODUCTION' is used when building the static version of storybook.
-
         config.module.rules = config.module.rules.filter(rule => {
             if (!rule.test) return true;
             return !rule.test.test(".scss");
         });
+        config.module.rules.push({
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+            }
+        });
+        config.module.rules.push({
+            test: /\.scss$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ],
+        });
+
+        config.resolve.extensions.push('.js', '.jsx', '.ts', '.tsx');
 
         return config;
     },
