d59efed68577ee939a6c290f8062f0f234a6c155
Will Eastcott <will@playcanvas.com>
2025-12-04 21:18:04 +0000

    Lint fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
 
 import babelParser from '@babel/eslint-parser';
 import playcanvasConfig from '@playcanvas/eslint-config';
+import storybook from 'eslint-plugin-storybook';
 import globals from 'globals';
 
 export default [...playcanvasConfig, {
@@ -20,4 +20,4 @@ export default [...playcanvasConfig, {
             ...globals.node
         }
     }
-}, ...storybook.configs["flat/recommended"]];
+}, ...storybook.configs['flat/recommended']];
