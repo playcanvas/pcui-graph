2db5fbf0b4b31addf0aef83f5862bcac2654482a
Will Eastcott <will@playcanvas.com>
2025-12-04 21:18:51 +0000

    Remove comment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,3 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-
 import babelParser from '@babel/eslint-parser';
 import playcanvasConfig from '@playcanvas/eslint-config';
 import storybook from 'eslint-plugin-storybook';
